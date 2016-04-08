7d16775b3f17e64bc7acdec0f346192cadb48b95
Jyothi Babu Araja <arajajyothibabu@gmail.com>
2016-04-08 13:23:53 +0530

    no use seen in applying multithreading also

--- EmailFilter/emailFilter.py
+++ EmailFilter/emailFilter.py
@@ -1,11 +1,12 @@
 from validate_email import validate_email
+import threading
 
-
-class EmailFilter:
+class EmailFilter():
     inputFileName = "emails.txt"
     outputFileName = "filteredEmails.txt"
     inputFile = file
     outputFile = file
+    expiredMails = 0
 
     def __init__(self, inputFileName, outputFileName):
         self.inputFile = file(inputFileName, "r")
@@ -23,18 +24,25 @@ class EmailFilter:
         except:
             return False
 
+    def updateMail(self, mail):
+        if self.mailExist(mail):
+            self.outputFile.write(mail + ",")
+        else:
+            self.expiredMails += 1
+
     def processMails(self):
         fileContent = self.inputFile.read()
         processedMails = set(fileContent.split(","))
+        print(processedMails)
         for mail in processedMails:
-            if self.mailExist(mail):
-               self.outputFile.write(mail + ",")
+            self.updateMail(mail.strip())
+        print("Total Expired Mails: ", self.expiredMails)
         return processedMails
 
 
 def main():
     emailFilter = EmailFilter()
-    emailFilter.processMails();
+    emailFilter.processMails()
     #or give your parameters
     #customEmailFilter = EmailFilter("inputfile.txt", "outputfile.txt")
     #customEmailFilter.processMails()
